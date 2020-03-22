cfaeddeeb04310c1564038f8271cbdf375bda050
Christopher Shoo <cristwinshoo@gmail.com>
2020-03-22 10:46:37 +0300

    initial commit

---
+++ src/main/java/com/fooddelivery/fooddeliveryapi/FooddeliveryapiApplication.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.fooddeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddeliveryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddeliveryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fooddelivery/fooddeliveryapi/FooddeliveryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.fooddeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddeliveryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
